4115faadbf029d11f5be2c8185c3fa3a42658d2b
Zaineb Louihi <shahdamine@gmail.com>
2021-06-22 22:54:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const circle=document.querySelector('#navbar .navbar .navlist .circle');
+const list=document.querySelector('#navbar .navbar .navlist');
+
+
+
+
+
+circle.addEventListener('click',()=>{
+    list.classList.toggle('open');
+    circle.classList.toggle('active');
+
+});
+
+
+
+
+
+
+
+
+
+
